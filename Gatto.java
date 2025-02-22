f1abd5885358a1de98e1b0ba940f6c8ec0eae71d
christian07b <98417286+christian07b@users.noreply.github.com>
2025-02-22 16:49:18 +0100

    Add files via upload

--- Gatto.java
+++ Gatto.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 /**
  * @author Bellomo Christian
  * questa classe serve per creare un oggetto di tipo gatto
